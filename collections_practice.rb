e7dc1673f438610ab5c25315199e6f6b0ea77be9
Nicky Liu <nickyliu91@gmail.com>
2018-08-28 05:56:31 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -29,3 +29,18 @@ def sort_array_desc(array)
   end
   array
 end
+
+def sort_array_char_count
+  i = 0
+  while i < array.length - 1
+    if array[i].length > array[i + 1].length
+      value = array[i]
+      array[i] = array[i + 1]
+      array[i + 1] = value
+      i = 0
+    else
+      i += 1
+    end
+  end
+  array
+end
