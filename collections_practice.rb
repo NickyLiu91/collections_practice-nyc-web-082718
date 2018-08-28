2611a9ec06d159b0a29b6f5b1f605dc5fda5443f
Nicky Liu <nickyliu91@gmail.com>
2018-08-28 05:32:01 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,7 +2,7 @@
 def sort_array_asc(array)
   i = 0
   while i < array.length - 1
-    if array[i] < array[i + 1]
+    if array[i] > array[i + 1]
       value = array[i]
       array[i] = array[i + 1]
       array[i + 1] = value
