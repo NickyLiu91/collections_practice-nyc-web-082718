f408c6904843641f8adb002efc02cec08dcae4ef
Nicky Liu <nickyliu91@gmail.com>
2018-08-28 05:55:05 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -13,3 +13,19 @@ def sort_array_asc(array)
   end
   array
 end
+
+
+def sort_array_desc(array)
+  i = 0
+  while i < array.length - 1
+    if array[i] < array[i + 1]
+      value = array[i]
+      array[i] = array[i + 1]
+      array[i + 1] = value
+      i = 0
+    else
+      i += 1
+    end
+  end
+  array
+end
